5f06f75760fa11e1a1233d3fc6fb48138497e1d0
Julian Cuello <julianc_69@hotmail.com>
2025-10-19 10:44:25 -0300

    trabajo casi terminado, solo falta acomodar boton modificar y agregar

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# Catálogo app de peliculas  - Web 2 UNICEN
+# Catálogo app de pelicula  - Web 2 UNICEN
 
-App de peliculas realizado utilzando Serve Side Programming con PHP.
+App de pelicula realizado utilzando Serve Side Programming con PHP.
 
-La idea de este proyecto es armar un catalogo de peliculas. 
+La idea de este proyecto es armar un catalogo de pelicula. 
 
 
 Integrantes: Cuello Julian DNI:33367562 y jessica Gimenez DNI: 36566991
 
-Tematica: Aplicacion de peliculas:
+Tematica: Aplicacion de pelicula:
 
 
-Descripcion tematica: Se penso realizar una aplicacion de peliculas para que el usuario vea un listado de actualizado de las mismas y de los actores que participaron en ella. La idea del proyecto es que se pueda acceder a las dos tablas para lograr interactuar con los datos de cada pelicula y de los actores que aparecen en ella, pudiendo realizar las distintas tareas que se solicitan en los trabajos practicos que nos brinda la catedra: borrar, modificar, agregar, mirar etc. 
+Descripcion tematica: Se penso realizar una aplicacion de pelicula para que el usuario vea un listaado de actualizado de las mismas y de los actores que participaron en ella. La idea del proyecto es que se pueda acceder a las dos tablas para lograr interactuar con los datos de cada pelicula y de los actores que aparecen en ella, pudiendo realizar las distintas tareas que se solicitan en los trabajos practicos que nos brinda la catedra: borrar, modificar, agregar, mirar etc. 
 
 
-Adjunto se encuentra la relacion entre las tablas: peliculas y actores.
+Adjunto se encuentra la relacion entre las tablas: pelicula y actores.
--- router.php
+++ router.php
@@ -1,47 +1,115 @@
 <?php
-require_once './app/controlador/pelicula_controlador.php';
 require_once './config.php';
+require_once './app/controllers/pelicula.controller.php';
+require_once './app/controllers/actor.controller.php';
+require_once './app/controllers/auth.controller.php';
+require_once './app/controllers/show.controller.php';
+
 
 define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
 
-// Instanciamos el controlador
-$controlador = new PeliculaControlador();
+$action = 'lista'; // accion por defecto
 
-$action = 'listar'; // Acción por defecto
 if (!empty($_GET['action'])) {
     $action = $_GET['action'];
 }
 
+/*
+  tabla ruteo                controller                                          descripc                                    
+| lista                   -> |peliculacontroller    |mostrar_pelicula()              |listaa productos                       |
+| listaId/:id             -> |peliculacontroller    |mostrar_pelicula_por_id(id)        |listaa producto por id                 |
+| eliminar_pelicula/:id         -> |peliculacontroller    |eliminar_pelicula(id)          |elimina registro de item              |
+| modificar_pelicula_formulario/:id     -> |peliculacontroller    |mostrar_formulario_modificacion(id)      |redirige a formulario de modificacion |
+| modificar_pelicula             -> |peliculacontroller    |mostrar_modificacion()            |envia formulario con modificacion     |
+| agregar_pelicula_formulario            -> |peliculacontroller    |mostrar_formulario_alta()          |redirige a formulario alta producto   |
+| agregar_pelicula                -> |peliculacontroller    |agregar_pelicula()               |envia formulario y crea nuevo producto|
+|---------------------------|------------------|------------------------|--------------------------------------|
+| actor               -> |actorcontroller|mostrar_actor()          |listaa actores                      |
+| actorId/:id         -> |actorcontroller|mostrar_actor_por_id())     |listaa actor por id                |
+| remover_actor/:id     -> |actorcontroller|remover_actor()        |elimina registro de actor         |
+| modificar_actor_formulario/:id -> |actorcontroller|mostrar_formulario_actor_modificacion()|redirige a formulario de modificacion |
+| modificar_actor         -> |actorcontroller|mostrar_actor_modificacion()    |envia formulario con modificacion     |
+| agregar_actor_formulario        -> |actorcontroller|mostrar_formulario_actor()      |redirige a formulario alta actor  |
+| agregar_actor            -> |actorcontroller|agregar_actor()           |envia formulario ,crea nueva actor|
+|---------------------------|------------------|------------------------|--------------------------------------|
+| login                  -> |Authcontroller    |showLogin()             |                                      |
+| logout                 -> |Authcontroller    |showLogout()            |                                      |
+*/
+
 $params = explode('/', $action);
 
+//instancio una sola vez
+$peliculacontroller = new peliculacontroller();
+$actorcontroller = new actorcontroller();
+$authcontroller = new Authcontroller();
+$mostrar_controller = new mostrar_controller();
+
 switch ($params[0]) {
-    //esto muestras las peliculas.
-    case 'listar':
-        $controlador->mostrar_peliculas();
+
+    case 'lista':
+        $peliculacontroller->mostrar_pelicula();
         break;
-        //aca lo que hace es poner en funcionamiento el boton "leer mas"
-    case 'pelicula':
-        if (!empty($params[1])) {
-            $controlador->seleccionar_pelicula($params[1]);
-        } else {
-            echo "ID de película no proporcionado.";
-        }
+    case 'listaId':
+        if(isset($params[1]))
+        $peliculacontroller->mostrar_pelicula_por_id($params[1]);
+        else $peliculacontroller->mostrar_pelicula();
         break;
     case 'eliminar_pelicula':
-            $controlador->eliminar_pelicula($params[1]);
-            break;
-    case 'formulario_agregar_pelicula':
-        $controlador->mostrar_formulario_alta();
+        if(isset($params[1]))
+        $peliculacontroller->eliminar_pelicula($params[1]);
+        else $mostrar_controller->mostrar_error("404-Not-Found");
+        break;
+    case 'modificar_pelicula_formulario':
+        if(isset($params[1]))
+        $peliculacontroller->mostrar_formulario_modificacion($params[1]);
+        else $mostrar_controller->mostrar_error("404-Not-Found");
+        break;
+    case 'modificar_pelicula':
+        $peliculacontroller->mostrar_modificacion();
+        break;
+    case 'agregar_pelicula_formulario':
+        $peliculacontroller->mostrar_formulario_alta();
         break;
     case 'agregar_pelicula':
-        $controlador->agregar_pelicula();
+        $peliculacontroller->agregar_pelicula();
         break;
-        echo "<h1>404 - Página no encontrada</h1>";
+    case 'actor':
+        $actorcontroller->mostrar_actor();
         break;
-        case 'editar_pelicula':
-        $controlador->editar_pelicula($params[1]);
+    case 'actorId':
+        if(isset($params[1]))
+        $actorcontroller->mostrar_actor_por_id($params[1]);
+        else $actorcontroller->mostrar_actor();
         break;
-    case 'mostrar_editar_pelicula':    
-        $controlador->editar_pelicula($params[1]);
-           break;
+    case 'remover_actor':
+        if(isset($params[1]))
+        $actorcontroller->remover_actor($params[1]);
+        else $mostrar_controller->mostrar_error("404-Not-Found");
+        break;
+    case 'modificar_actor_formulario':
+        if(isset($params[1]))
+        $actorcontroller->mostrar_formulario_actor_modificacion($params[1]);
+        else $mostrar_controller->mostrar_error("404-Not-Found");
+        break;
+    case 'modificar_actor':
+        $actorcontroller->mostrar_actor_modificacion();
+        break;
+    case 'agregar_actor_formulario':
+        $actorcontroller->mostrar_formulario_actor();
+        break;
+    case 'agregar_actor':
+        $actorcontroller->agregar_actor();
+        break;
+    case 'login':
+        $authcontroller->showLogin();
+        break;
+    case 'logout':
+        $authcontroller->logout();
+        break;
+    case 'auth':
+        $authcontroller->auth();
+        break;
+    default:
+        $mostrar_controller->mostrar_error("404-Not-Found");
+        break;
 }
